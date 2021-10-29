8aed6da4c8581af2121910bed7ce4db77941757c
Alexoko12 <27732632+Alexoko12@users.noreply.github.com>
2021-10-29 20:53:41 +0200

    Add 2.0.9.2

---
+++ Marlin-2.0.9.2/buildroot/share/PlatformIO/scripts/mks_robin_nano35.py
@@ -0,0 +1,6 @@
+#
+# buildroot/share/PlatformIO/scripts/mks_robin_nano35.py
+#
+import robin
+#robin.prepare("0x08007000", "mks_robin_nano.ld", "Robin_nano35.bin")
+robin.prepare("0x08007000", "mks_robin_nano.ld", "Lotmaxx.bin")
